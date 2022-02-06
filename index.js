493894133c3d007fd2ab026170592d8aa577a9a9
xoltia <38849891+xoltia@users.noreply.github.com>
2022-02-05 20:50:39 -0600

    Switch to express
    Will make implementing features later on (verification, etc) easier

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 require('dotenv').config();
+const express = require('express');
+const { query } = require('express-validator');
 const path = require('path');
 const jmdict = require('./jmdict');
 const { search, count } = require('./api');
 const { parse } = require('./mecab');
 
-const fastify = require('fastify')({
-    logger: true,
-    maxParamLength: 250,
-});
+const app = express();
 
 // If environment set to production serve built client files
 if (process.env.NODE_ENV === 'production') {
-    fastify.register(require('fastify-static'), {
-        root: path.join(__dirname, '/client/dist'),
-    });
+    app.use(express.static(path.join(__dirname, '/client/dist')));
 }
 
-fastify.get('/api/tags', function(request, reply) {
-    reply.send(jmdict.tags);
-});
+app.get('/api/tags',
+    (req, res) => res.json(jmdict.tags)
+);
 
-fastify.get('/api/wakachi/:phrase', function(request, reply) {
-    parse(request.params.phrase, (result) => {
-        reply.send(result.map(data =>
+app.get('/api/wakachi/:phrase', (req, res) => {
+    parse(req.params.phrase, (result) => {
+        res.json(result.map(data =>
             [
                 data[0], // Matched text
                 data[1]['品詞'] == '記号' ? null : data[1]['原形'], // Standard form or null if symbol
@@ -31,25 +28,20 @@ fastify.get('/api/wakachi/:phrase', function(request, reply) {
     });
 });
 
-fastify.get('/api/count/:word', function (request, reply) {
-    reply.send(count(request.params.word));
-});
+app.get('/api/count/:word',
+    (req, res) => res.json(count(req.params.word))
+);
 
-fastify.get('/api/define/:word', {
-    schema: {
-        querystring: {
-            offset: { type: 'integer' },
-            size: { type: 'integer' }
-        }
-    }
-}, function (request, reply) {
-    // Page number
-    const page = request.query.page || 0;
-    // Page size, default to max page size if page not specified or if invalid size (<0)
-    const size = request.query.size && request.query.size > 0 ? request.query.size : (process.env.MAX_PAGE_SIZE || 25);
-
-    reply.send(search(request.params.word, { page, size }));
-});
+app.get('/api/define/:word', 
+    query('page').isInt().default(0),
+    query('size').isInt({ min: 1 }).default(process.env.MAX_PAGE_SIZE || 25),
+    (req, res) => res.json(
+        search(req.params.word, {
+            page: req.query.page,
+            size: req.query.size 
+        })
+    )
+);
 
 // Set up JMDict module before starting server or search functions will not work
 jmdict.setup(
@@ -59,7 +51,7 @@ jmdict.setup(
 );
 
 // Port must be 3080 to work with client when running from dev server
-fastify.listen(process.env.PORT || 3080, function (err) {
+app.listen(process.env.PORT || 3080, function(err) {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
